2800778a6e50d1de0224d6f1b3820263d55be7f8
zhechu <ling-yu-wang@qq.com>
2019-08-17 11:29:16 +0800

    优化

--- spring-cloud-gateway-server/src/main/java/com/wise/GatewayApplication.java
+++ spring-cloud-gateway-server/src/main/java/com/wise/GatewayApplication.java
@@ -2,10 +2,6 @@ package com.wise;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.gateway.filter.ratelimit.KeyResolver;
-import org.springframework.context.annotation.Bean;
-import reactor.core.publisher.Mono;
 
 /**
  * 启动类
@@ -16,15 +12,6 @@ public class GatewayApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(GatewayApplication.class, args);
-    }
-
-    /**
-     * 根据 IP 进行限流
-     * @return
-     */
-    @Bean
-    public KeyResolver ipKeyResolver() {
-        return exchange -> Mono.just(exchange.getRequest().getRemoteAddress().getHostName());
     }
 
 }
